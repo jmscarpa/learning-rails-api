54fe418d3cfb9a517a7af02eb119ffc65a44abc4
João Mateus Scarpa <joao.scarpa@gmail.com>
2023-10-03 18:34:40 -0300

    Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,5 @@
-Category.create(name: 'Ruby')
+Post.destroy_all
+User.destroy_all
+
+User.create(username: 'jmscarpa', bio: "Programador dodói da cabeça")
+User.create(username: 'unversed', bio: "Futuro estagiário senior")
